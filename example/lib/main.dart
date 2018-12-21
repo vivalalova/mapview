116fe5dcc2275a600425c2bac9cf6e000e4a7dbd
vivalalova <vivalalova0@gmail.com>
2018-12-21 11:42:29 +0800

    re

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
 
+  var mapview = FlutterMapview();
+
   @override
   void initState() {
     super.initState();
--- lib/flutter_mapview.dart
+++ lib/flutter_mapview.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterMapview {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_mapview');
+  static const MethodChannel _channel = const MethodChannel('flutter_mapview');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
